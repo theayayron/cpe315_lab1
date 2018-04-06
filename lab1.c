776e8c50fb69c85cf9d1bf7d34c33979cfa06a4b
csettles <caitsettles@gmail.com>
2018-04-06 09:45:22 -0700

    added comments

--- lab1.c
+++ lab1.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 void add_bytes(unsigned char a, unsigned char b);
-
 void add_bytes_carry(unsigned char a, unsigned char b);
-
 void add_bytes_overflow(char a, char b);
-
 unsigned int arbitrary_byte_add(unsigned char *result, unsigned char *a1,
                                 unsigned char *a2, int size,
                                 unsigned int carry_in);
-
 void print_array(unsigned char *a, int size);
-
 void byteOrdering(void);
 
 int main(int argc, char *argv[]) {
@@ -74,9 +69,10 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-/* void -> void 
- * creates a byte array and accesses the array as short
- * integers, integers, and long integers. */
+/**
+ * Creates a byte array and accesses the array as short
+ * integers, integers, and long integers.
+ */
 void byteOrdering(void) {
   int i;
   unsigned char bytestring[] = {
@@ -159,18 +155,38 @@ void byteOrdering(void) {
   printf("========================\n");
 }
 
+/**
+ * Adds two bytes together and prints the sum. The sum is one byte,
+ * ignoring any overflow.
+ * @param a the first byte to add
+ * @param b the second byte to add
+ */
 void add_bytes(unsigned char a, unsigned char b) {
     unsigned char sum;
     sum = a + b;
     printf("0x%02x + 0x%02x = 0x%02x\n", a, b, sum);
 }
 
+/**
+ * Adds two bytes together and determines if a carry condition occurred.
+ * This means that the result was bigger than the maximum value of a byte (255).
+ * @param a the first byte to add
+ * @param b the second byte to add
+ */
 void add_bytes_carry(unsigned char a, unsigned char b) {
-    int sum;
+    int sum; /* store in an integer so sum can hold values greater than 255 */
     sum = a + b;
     printf("0x%02x + 0x%02x Carry: %d\n", a, b, sum > 255);
 }
 
+/**
+ * Adds two bytes together and determines if an overflow condition occurred.
+ * There are two cases where this can occur: when both operands are positive
+ * and the result is negative, or when both operands are negative and the
+ * result is positive.
+ * @param a the first byte to add
+ * @param b the second byte to add
+ */
 void add_bytes_overflow(char a, char b) {
     char sum;
     sum = a + b;
@@ -179,6 +195,18 @@ void add_bytes_overflow(char a, char b) {
     printf("%d\n", (a < 0 && b < 0 && sum > 0) || (a > 0 && b > 0 && sum < 0));
 }
 
+/**
+ * Adds two arrays of bytes and stores the result in the given array.
+ * It also factors in a carry flag which will affect the rightmost
+ * byte pair and propagate through to the leftmost byte pair before
+ * being returned.
+ * @param result the array where the resulting sums will be stored
+ * @param a1 the first array of bytes to add
+ * @param a2 the second array of bytes to add
+ * @param size the number of bytes in each array
+ * @param carry_in the carry value to the rightmost byte pair
+ * @return the carry from the leftmost byte pair
+ */
 unsigned int arbitrary_byte_add(unsigned char *result, unsigned char *a1,
                                 unsigned char *a2, int size,
                                 unsigned int carry_in) {
@@ -192,15 +220,21 @@ unsigned int arbitrary_byte_add(unsigned char *result, unsigned char *a1,
     print_array(a2, size);
     printf("\n=\t");
 
-    for (i = size - 1; i >= 0; i--) {
+    for (i = size - 1; i >= 0; i--) { /* start at the right */
         sum = a1[i] + a2[i] + carry;
         result[i] = (unsigned char) sum;
-        carry = sum >> 8;
+        carry = sum >> 8; /* shift away lower byte to get carry value */
     }
     print_array(result, size);
     return (unsigned int) carry;
 }
 
+/**
+ * A helper function for arbitrary_byte_add() that prints an
+ * array of size bytes.
+ * @param a the byte array to print
+ * @param size the number of bytes in a
+ */
 void print_array(unsigned char *a, int size) {
     int i = 0;
     while (i < size) {
